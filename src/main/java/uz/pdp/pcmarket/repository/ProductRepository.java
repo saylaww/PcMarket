89de23f12733048b6f2e36a5cdf428bee03008da
saylaww <saylaww@gmail.com>
2021-04-01 18:28:11 +0500

    success 4

--- src/main/java/uz/pdp/pcmarket/repository/ProductRepository.java
+++ src/main/java/uz/pdp/pcmarket/repository/ProductRepository.java
@@ -9,12 +9,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import uz.pdp.pcmarket.entity.Product;
 import uz.pdp.pcmarket.projection.CustomProduct;
 
+import java.util.List;
 import java.util.Optional;
 
 @RepositoryRestResource(path = "product", excerptProjection = CustomProduct.class)
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+    @RestResource(path = "byPrice")
+    List<Product> findAllByPriceBetween(@RequestParam("start") Integer start,@RequestParam("end") Integer end);
+
     @RestResource(path = "byName")
     Optional<Product> findByName(@RequestParam("name") String name);
+
+    @RestResource(path = "byCategory")
+    List<Product> findAllByCategory_Name(@RequestParam("category_name") String category_name);
+
 
 }
